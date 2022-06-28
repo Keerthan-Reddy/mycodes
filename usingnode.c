212132071b87627754fbfa1eff2c5c69b2ea7a68
Keerthan-Reddy <90082358+Keerthan-Reddy@users.noreply.github.com>
2022-06-28 14:11:00 +0530

    Add files via upload

---
+++ usingnode.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct node
+{
+    int data;
+    struct node* link;
+};
+
+struct node *start = NULL;
+struct node *newnode;
+
+void creat(){
+    newnode=((struct node*)malloc(sizeof(struct node)));
+    printf("enter the data");
+    scanf("%d",&newnode->data);
+    newnode->link=NULL;
+
+}
+void display(){
+printf("%d",newnode->data);
+}
+
+void main(){
+    creat();
+    display();
+}
