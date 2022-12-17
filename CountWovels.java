6b21c5d1c9c2a9034a214f2801ae576f2dca2fcb
Keerthan-Reddy <90082358+Keerthan-Reddy@users.noreply.github.com>
2022-12-17 14:41:45 +0000

    change

---
+++ CountWovels.java
@@ -0,0 +1,23 @@
+import java.util.*;
+class CountWovels{
+    public static void main(String args[]){  
+        Scanner sc =new Scanner(System.in);
+        String s;
+        int n;
+       
+        s=sc.next();
+        
+        int count=0;
+      
+        
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='a'|| s.charAt(i)=='e' || s.charAt(i)=='i' || s.charAt(i)=='o' || s.charAt(i)=='u' ){
+              
+                count++;
+                
+            }
+            
+        }
+        System.out.println("the number of wovels are. "+count); 
+    }
+}
