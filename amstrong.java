bd6a053b04e4e163bc28ed7b23ff2079ec3d5e62
Keerthan-Reddy <90082358+Keerthan-Reddy@users.noreply.github.com>
2022-12-22 17:47:19 +0000

    practice on week one

---
+++ amstrong.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class amstrong {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number"); 
+        int n = sc.nextInt();
+        int s=0, r , m;
+        m=n; 
+        while(n > 0) {
+        r = n%10;
+        s = s + (r*r*r);
+        n = n/10;
+        }
+        if (s==m)
+        System.out.println("The number " +m + " -> is Amstrong");
+         else
+        System.out.println("The number " +m + " -> is Not Amstrong");
+        } }
